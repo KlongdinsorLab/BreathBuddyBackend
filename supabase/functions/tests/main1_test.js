8253fc0e229fd8ff95dc667ca5723eec64d6a9b9
EgapNimda <6434449023@student.chula.ac.th>
2024-07-23 16:11:45 +0700

    Delete Unused Function

--- supabase/functions/tests/main1_test.js
+++
@@ -1,7 +0,0 @@
-import test from 'node:test';
-import * as assert from 'node:assert';
-
-test('synchronous passing test 1', (t) => {
-  // This test passes because it does not throw an exception.
-  assert.strictEqual(1, 1);
-});
