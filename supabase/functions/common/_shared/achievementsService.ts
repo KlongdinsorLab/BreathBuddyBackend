61948ff7d91ebb230988624c7adc673325333926
EgapNimda <6434449023@student.chula.ac.th>
2024-07-25 14:21:38 +0700

    add get all achievements

--- supabase/functions/common/_shared/achievementsService.ts
+++ supabase/functions/common/_shared/achievementsService.ts
@@ -26,7 +26,6 @@ export async function getLockedAchievementsSelectCharacter/* or MyBag? */(player
 }
 
 export async function getLockedAchievementsScoreUpdate (playerId: number) {
-
     const achievements = await db.select({
         id: achievementsTable.id,
         name: achievementsTable.name,
@@ -44,65 +43,80 @@ export async function getLockedAchievementsScoreUpdate (playerId: number) {
 }
 
 export async function getLockedAchivementsEndGame (playerId: number) {
-
     const achievements = await db.select({
         id: achievementsTable.id,
         name: achievementsTable.name,
         detail: achievementsTable.detail
     }).from(achievementsTable)
-        .where(and(
-            notExists(db.select().from(playersAchievementsTable).where(and(
-                eq(playersAchievementsTable.achievement_id, achievementsTable.id),
-                eq(playersAchievementsTable.player_id, playerId )
-            ))),
-            or(
-                isNotNull(achievementsTable.games_played_in_a_day),
-                isNotNull(achievementsTable.games_played),
-                isNotNull(achievementsTable.games_played_consecutive_days)
-            )
+    .where(and(
+        notExists(db.select().from(playersAchievementsTable).where(and(
+            eq(playersAchievementsTable.achievement_id, achievementsTable.id),
+            eq(playersAchievementsTable.player_id, playerId )
+        ))),
+        or(
+            isNotNull(achievementsTable.games_played_in_a_day),
+            isNotNull(achievementsTable.games_played),
+            isNotNull(achievementsTable.games_played_consecutive_days)
+        )
             
-        ))
+    ))
 
     return achievements
 }
 
 export async function getLockedAchievementsBooster (playerId: number) {
-
     const achievements = await db.select({
         id: achievementsTable.id,
         name: achievementsTable.name,
         detail: achievementsTable.detail
     }).from(achievementsTable)
-        .where(and(
-            notExists(db.select().from(playersAchievementsTable).where(and(
-                eq(playersAchievementsTable.achievement_id, achievementsTable.id),
-                eq(playersAchievementsTable.player_id, playerId )
-            ))),
-            isNotNull(achievementsTable.boosters),
-            eq(achievementsTable.booster_action, 'gain')
-        ))
+    .where(and(
+        notExists(db.select().from(playersAchievementsTable).where(and(
+            eq(playersAchievementsTable.achievement_id, achievementsTable.id),
+            eq(playersAchievementsTable.player_id, playerId )
+        ))),
+        isNotNull(achievementsTable.boosters),
+        eq(achievementsTable.booster_action, 'gain')
+    ))
 
     return achievements
 }
 
 export async function getLockedAchievementsStartGame (playerId: number) {
-    
     const achievements = await db.select({
         id: achievementsTable.id,
         name: achievementsTable.name,
         detail: achievementsTable.detail
     }).from(achievementsTable)
-        .where(and(
-            notExists(db.select().from(playersAchievementsTable).where(and(
-                eq(playersAchievementsTable.achievement_id, achievementsTable.id),
-                eq(playersAchievementsTable.player_id, playerId )
-            ))),
-            isNotNull(achievementsTable.boss_encounter),
-            and(
-                isNotNull(achievementsTable.boosters),
-                eq(achievementsTable.booster_action,"use")
-            )
-        ))
+    .where(and(
+        notExists(db.select().from(playersAchievementsTable).where(and(
+            eq(playersAchievementsTable.achievement_id, achievementsTable.id),
+            eq(playersAchievementsTable.player_id, playerId )
+        ))),
+        isNotNull(achievementsTable.boss_encounter),
+        and(
+            isNotNull(achievementsTable.boosters),
+            eq(achievementsTable.booster_action,"use")
+        )
+    ))
+
+    return achievements
+}
+
+export async function getUnlockedAchievements (playerId: number) { // for My Bag page
+    const achievements = await db.select({
+        id: achievementsTable.id,
+        name: achievementsTable.name,
+        detail: achievementsTable.detail
+    }).from(achievementsTable)
+    .innerJoin(playersAchievementsTable, eq(playersAchievementsTable.achievement_id, achievementsTable.id))
+    .where(eq(playersAchievementsTable.player_id, playerId))
+
+    return achievements
+}
+
+export async function getAllAchievements () {
+    const achievements = await db.select().from(achievementsTable)
 
     return achievements
 }
