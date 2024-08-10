44eedcaceea06404eaf05ec0c7eaae61d828b3ad
EgapNimda <6434449023@student.chula.ac.th>
2024-08-10 11:48:12 +0700

    Auth Service

---
+++ supabase/functions/login/index.ts
@@ -0,0 +1,54 @@
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+
+// Setup type definitions for built-in Supabase Runtime APIs
+import "jsr:@supabase/functions-js/edge-runtime.d.ts"
+import { getFirebaseId, login } from "../common/_shared/authService.ts";
+
+console.log("Hello from Functions!")
+
+Deno.serve(async (req) => {
+  try{
+    const { phoneNumber } = await req.json()
+    const authHeader = req.headers.get("Authorization")!
+    const firebaseId = getFirebaseId(authHeader)
+
+    if(phoneNumber !== phoneNumber(authHeader)) {
+      throw new Error("Authentication Error")
+    }
+  
+    await login(firebaseId, phoneNumber)
+
+    const response = {
+      message : "Ok",
+      response : authHeader.replace("Bearer ","")
+    }
+    return new Response(
+      JSON.stringify(response),
+      { headers: { "Content-Type": "application/json" } },
+    )
+  }
+  catch(error){
+    const response = {
+      message : error.message,
+    }
+    return new Response(
+      JSON.stringify(response),
+      { headers: { "Content-Type": "application/json" } },
+    )
+  }
+  
+})
+
+/* To invoke locally:
+
+  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
+  2. Make an HTTP request:
+
+  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/login' \
+    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+    --header 'Content-Type: application/json' \
+    --data '{"name":"Functions"}'
+
+*/
