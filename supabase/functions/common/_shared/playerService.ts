e8906624b872d6078c951abb54281c3049d85713
EgapNimda <6434449023@student.chula.ac.th>
2024-08-17 10:43:05 +0700

    add player service

--- supabase/functions/common/_shared/playerService.ts
+++ supabase/functions/common/_shared/playerService.ts
@@ -1,6 +1,7 @@
-import { and, eq } from "npm:drizzle-orm@^0.31.4/expressions";
+import { and, desc, eq, lt } from "npm:drizzle-orm@^0.31.4/expressions";
 import { db } from "../db.ts";
-import { playersCharactersTable, playersTable } from "../schema.ts";
+import { charactersTable, gameSessionsTable, levelsTable, playersAchievementsTable, playersCharactersTable, playersTable } from "../schema.ts";
+import { takeUniqueOrThrow } from "./takeUniqueOrThrow.ts";
 
 export async function updateAirflow(playerId : number, airflow : number){
     if(airflow < 100 || airflow > 600 || airflow%100 !== 0) {
@@ -34,4 +35,38 @@ export async function updateSelectedCharacter(playerId : number, characterId : n
     }
 
     await db.update(playersTable).set({selected_character_id : characterId}).where(eq(playersTable.id,playerId))
+}
+
+export async function getLevel(playerId : number) {
+    const allGameSessions = await db.select().from(gameSessionsTable).where(eq(gameSessionsTable.player_id,playerId))
+
+    let totalScore = 0
+    allGameSessions.forEach((gameSession) => {
+        totalScore = totalScore + (gameSession.score === null ? 0 : gameSession.score)
+    })
+
+    const level = await db.select().from(levelsTable).where(lt(levelsTable.score_required,totalScore)).orderBy(desc(levelsTable.score_required))
+    return level[0].level
+}
+
+export async function unlockCharacter(playerId : number, characterId : number){
+    const achievementsList = await db.select().from(playersAchievementsTable).where(eq(playersAchievementsTable.player_id,playerId))
+    const achievementsNumber = achievementsList.length
+
+    const character = await db.select().from(charactersTable).where(eq(charactersTable.id,characterId)).then(takeUniqueOrThrow)
+
+    if(achievementsNumber < character.achievement_number_required) {
+        throw new Error("Achievements Number Required not met")
+    }
+
+    await db.insert(playersCharactersTable).values({
+        player_id : playerId,
+        character_id : characterId
+    })
+}
+
+export async function getUnlockedCharacters(playerId : number) {
+    const unlockedCharacters = await db.select().from(playersCharactersTable).where(eq(playersCharactersTable.player_id,playerId))
+
+    return unlockedCharacters
 }
