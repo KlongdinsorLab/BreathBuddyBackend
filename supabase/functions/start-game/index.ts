a1ad928370703552b1a616a4105c2cf46af6664b
Chanathip Chuehome <stamp1234561@gmail.com>
2025-01-25 16:01:43 +0700

    Reapply "fix: json format for request and response log"
    This reverts commit 0198165b300a9926a667b5a9feb41237c66f954d.

--- supabase/functions/start-game/index.ts
+++ supabase/functions/start-game/index.ts
@@ -53,7 +53,9 @@ Deno.serve(async (req) => {
     const response = { message: "Ok", response: result };
 
     logger.info(
-      `API call to ${loggedRequest.url} with method ${loggedRequest.method}. Data modification performed. Request details: ${loggedRequest.json()}`,
+      `API call to ${loggedRequest.url} with method ${loggedRequest.method}. Data modification performed. Request details: ${
+        JSON.stringify(loggedRequest.json())
+      }`,
     );
 
     return new Response(JSON.stringify(response), {
