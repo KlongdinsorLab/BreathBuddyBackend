70690c7abea3883157f4ab745e11983ffea5158d
Chanathip Chuehome <stamp1234561@gmail.com>
2025-01-25 15:58:27 +0700

    Revert "feat: add sentry context for http error"
    This reverts commit 7389ce840cacb95be8e3444ac653c67cd4384bb7.

--- supabase/functions/unlock-character/index.ts
+++ supabase/functions/unlock-character/index.ts
@@ -62,12 +62,6 @@ Deno.serve(async (req) => {
     });
   } catch (error) {
     Sentry.captureException(error);
-    Sentry.setContext("http", {
-      method: req.method,
-      url: req.url,
-      headers: Object.fromEntries(req.headers.entries()),
-    });
-
     logger.error("Error occurred while processing request", error);
 
     const response = {
--- supabase/functions/update-selected-character/index.ts
+++ supabase/functions/update-selected-character/index.ts
@@ -63,11 +63,6 @@ Deno.serve(async (req) => {
   } catch (error) {
     logger.error("Error occurred while processing request", error);
     Sentry.captureException(error);
-    Sentry.setContext("http", {
-      method: req.method,
-      url: req.url,
-      headers: Object.fromEntries(req.headers.entries()),
-    });
 
     const response = { message: error.message };
 
